da5e61be672b41b8eb7f0328832369da1327778d
Bogdan Radu <bogdanrn@gmail.com>
2018-06-19 19:54:52 +0300

    added python to apt-get

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,73 +3,7 @@
 # echo "FROM buildpack-deps:$(awk -F'_' '{print tolower($2)}' <<< $LINUX_VERSION)"
 echo "FROM circleci/node:8.11.3"
 echo "RUN sudo apt-get update"
-echo "RUN sudo apt-get -y install zip lsb-release unzip"
-
-# if [ ! -e $RUBY_VERSION_NUM ] ; then
-#     echo "RUN sudo apt-get install -y libssl-dev && wget http://ftp.ruby-lang.org/pub/ruby/$(awk -F'.' '{ print $1"."$2 }' <<< $RUBY_VERSION_NUM)/ruby-$RUBY_VERSION_NUM.tar.gz && \
-#     tar -xzvf ruby-$RUBY_VERSION_NUM.tar.gz && \
-#     cd ruby-$RUBY_VERSION_NUM/ && \
-#     ./configure --disable-install-doc && \
-#     make -j4 && \
-#     make install && \
-#     ruby -v"
-# fi
-
-# if [ ! -e $NODE_VERSION_NUM ] ; then
-#     echo "RUN wget https://nodejs.org/dist/v$NODE_VERSION_NUM/node-v$NODE_VERSION_NUM.tar.gz && \
-#     tar -xzvf node-v$NODE_VERSION_NUM.tar.gz && \
-#     rm node-v$NODE_VERSION_NUM.tar.gz && \
-#     cd node-v$NODE_VERSION_NUM && \
-#     ./configure && \
-#     make -j4 && \
-#     make install && \
-#     cd .. && \
-#     rm -r node-v$NODE_VERSION_NUM"
-# fi
-
-if [ ! -e $PYTHON_VERSION_NUM ] ; then
-    echo "RUN wget https://www.python.org/ftp/python/$PYTHON_VERSION_NUM/Python-$PYTHON_VERSION_NUM.tgz && \
-    tar xzf Python-$PYTHON_VERSION_NUM.tgz && \
-    rm Python-$PYTHON_VERSION_NUM.tgz && \
-    cd Python-$PYTHON_VERSION_NUM && \
-    ./configure && \
-    make install"
-
-    echo "RUN curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
-            python get-pip.py
-            "
-fi
-
-# if [ ! -e $PHP_VERSION_NUM ] ; then
-#     wget "http://php.net/distributions/php-${PHP_VERSION_NUM}.tar.xz"
-# fi
-
-if [ $JAVA = "true" ] ; then
-cat << EOF
-RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
-    echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
-    sudo apt-get update && sudo apt-get -y install -t jessie-backports openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="9"' /etc/os-release) ] ; then \\
-		sudo apt-get update && sudo apt-get -y -q --no-install-recommends install -t stable openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="14.04"' /etc/os-release) ] ; then \\
-		apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; elif [ \$(grep 'VERSION_ID="16.04"' /etc/os-release) ] ; then \\
-    apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; fi
-EOF
-fi
+echo "RUN sudo apt-get -y install zip lsb-release unzip python-dev python-pip"
 
 if [ $MYSQL_CLIENT = "true" ] ; then
     echo "RUN sudo apt-get -y install mysql-client"
@@ -77,16 +11,6 @@ fi
 
 if [ $POSTGRES_CLIENT = "true" ] ; then
     echo "RUN sudo apt-get -y install postgresql-client"
-fi
-
-if [ $DOCKERIZE = "true" ] ; then
-DOCKERIZE_VERSION="v0.6.1"
-
-cat << EOF
-RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && \\
-    tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && \\
-    rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
-EOF
 fi
 
 # install bats for testing
