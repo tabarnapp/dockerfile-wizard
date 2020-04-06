34ddc1f898e6b52f0f5a969e8b048543e3b6c4e8
Bogdan Radu <bogdanrn@gmail.com>
2020-04-06 17:40:41 +0300

    added aws cli to it

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,7 +4,20 @@
 echo "FROM circleci/node:12.16.1"
 echo "RUN sudo apt-get update"
 echo "RUN sudo apt-get -y install zip lsb-release unzip python-dev python-pip"
-
+echo "ENV AWS_CLI_VERSION=2.0.6"
+echo "RUN curl -sSL \"https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${AWS_CLI_VERSION}.zip\" -o \"aws-cli.zip\" && \
+	unzip aws-cli.zip && \
+	sudo ./aws/install && \
+	rm -r aws-cli.zip aws && \
+	aws --version"
+echo "RUN sudo apt-get update && sudo apt-get install \
+		python3 \
+		python3-pip \
+    && \
+    sudo rm -rf /var/lib/apt/lists/* && \
+    pip3 --no-cache-dir install awsebcli && \
+    eb --version"
+  
 if [ $MYSQL_CLIENT = "true" ] ; then
     echo "RUN sudo apt-get -y install mysql-client"
 fi
